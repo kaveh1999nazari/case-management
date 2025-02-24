81aeaa9f36be8fea0ae9cf090d82340c18efe3ee
kaveh <kaveh1999nazari@gmail.com>
2025-02-24 10:09:14 +0330

    first commit

--- database/seeders/CreateCategorySeeder.php
+++ database/seeders/CreateCategorySeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class CreateCategorySeeder extends Seeder
@@ -12,6 +12,18 @@ class CreateCategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $names = [
+            'قاب موبایل',
+            'لیوان',
+            'لباس',
+            'دفتر',
+            'لبتاب'
+        ];
+        foreach ($names as $name) {
+            Category::query()
+                ->create([
+                    'name' => $name
+                ]);
+        }
     }
 }
