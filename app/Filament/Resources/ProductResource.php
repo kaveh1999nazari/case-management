967cd387ea0d1059b0f302cee180470219755f05
kaveh <kaveh1999nazari@gmail.com>
2025-04-08 10:29:01 +0330

    Add DeleteAction in actions

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -132,6 +132,7 @@ class ProductResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
