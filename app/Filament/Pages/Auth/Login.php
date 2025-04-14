cc6f95fb884bc8ac06fc9cbe92ea2143e8ae315c
kaveh <kaveh1999nazari@gmail.com>
2025-04-14 16:47:14 +0330

    make password more secure

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -25,6 +25,7 @@ class Login extends BaseLogin
 
                 TextInput::make('password')
                     ->label('رمز عبور')
+                    ->password()
                     ->required()
                     ->autocomplete('current-password'),
             ]);
